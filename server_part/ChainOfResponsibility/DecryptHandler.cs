6f613377e6550e805de82b02aa8ef23fdc0e08c2
Mikhail <m.f.katrin@mail.ru>
2015-05-20 23:37:01 +0300

    lab uploaded

---
+++ server_part/ChainOfResponsibility/DecryptHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server_part.ChainOfResponsibility
+{
+    public class DecryptHandler: Handler
+    {
+        public override void handle_request(int request, ClientInfo client)
+        {
+            if (request == 1)   //  расшифровка
+            {
+                ;
+            }
+            else if (successor != null)
+                successor.handle_request(request, client);
+        }
+    }
+}
---
+++ server_part/ChainOfResponsibility/EncryptHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server_part.ChainOfResponsibility
+{
+    public class EncryptHandler: Handler
+    {
+        public override void handle_request(int request, ClientInfo client)
+        {
+            if (request == 3)   //  зашифровка
+            {
+                ;
+            }
+            else if (successor != null)
+                successor.handle_request(request, client);
+        }
+    }
+}
